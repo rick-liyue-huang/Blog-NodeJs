0ed00bc3ee8ae14168e749aeb4b37105b856f070
RickHuang <rick.liyue.huang@gmail.com>
2019-11-26 13:43:37 +1100

    feat: add session on server app

--- Blog-New/src/app.js
+++ Blog-New/src/app.js
@@ -5,6 +5,9 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+const { REDIS_CONFIG } = require('./conf/db');
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -31,6 +34,24 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+// config the session and redis before using routers
+// session configuration
+app.keys = ['rick.liyue.huang@gmail.com'];
+
+app.use(session({
+  key: 'blog.sid', // cookie name default as koa.sid
+  prefix: 'blog:sess:', // redis key prefix default as koa:sess:
+  cookie: {
+    path: '/',
+    httpOnly: true, // only set cookie on server side
+    maxAge: 24 * 60 * 60 * 1000, // cookie valid time
+  },
+  ttl: 24 * 60 * 60 * 1000, // redis valid time default as maxAge 
+  store: redisStore({
+    all: `${REDIS_CONFIG.host}:${REDIS_CONFIG.port}`
+  })
+}))
 
 // routes
 app.use(index.routes(), index.allowedMethods())
