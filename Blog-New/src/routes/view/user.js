244a12ee25ea288bd01bd27bdc1aa4d4928d2992
RickHuang <rick.liyue.huang@gmail.com>
2019-11-29 11:26:37 +1100

    fix: add check the login register page

--- Blog-New/src/routes/view/user.js
+++ Blog-New/src/routes/view/user.js
@@ -6,12 +6,30 @@
 
 const router = require('koa-router')();
 
+/**
+ * 
+ * @param {Object} ctx koa2 ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false, // default as unlogin
+  }
+  const userInfo = ctx.session.userInfo;
+  if(userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+  return data;
+}
+
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx));
 });
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {});
+  await ctx.render('register', getLoginInfo(ctx));
 });
 
 module.exports = router;
